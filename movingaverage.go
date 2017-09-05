c34c600b8559281216b16306f100602c47289ff9
Luis Garcia <luis.a.garcia@hpe.com>
2017-09-05 16:28:41 -0600

    first commit

--- movingaverage.go
+++ movingaverage.go
@@ -1,34 +1,37 @@
 package movingstats
 
-import "math"
+import (
+	"math"
+
+	"github.com/lagarciag/ringbuffer"
+)
 
 type MovingAverage struct {
 	count  int
 	period int
 
-	avgSum      float64
-	average     float64
-	avgHistBuff *historyBuffer
+	avgSum  float64
+	average float64
+	//avgHistBuff *historyBuffer
+	avgHistBuff *ringbuffer.RingBuffer
 
 	avg2Sum     float64
 	variance    float64
-	varHistBuff *historyBuffer
-
+	varHistBuff *ringbuffer.RingBuffer
 }
 
 func NewAverage(period int) *MovingAverage {
 
 	avg := &MovingAverage{}
 	avg.period = period
-	avg.avgHistBuff = NewHistoryBuffer(period)
-	avg.varHistBuff = NewHistoryBuffer(period)
+	avg.avgHistBuff = ringbuffer.NewBuffer(period)
+	avg.varHistBuff = ringbuffer.NewBuffer(period)
 	return avg
 }
 
 func (avg *MovingAverage) Add(value float64) {
 	avg.avg(value)
 }
-
 
 func (avg *MovingAverage) SimpleMovingAverage() float64 {
 	return avg.average
@@ -41,7 +44,7 @@ func (avg *MovingAverage) MovingStandardDeviation() float64 {
 func (avg *MovingAverage) avg(value float64) {
 	avg.count++
 
-	lastAvgValue := avg.avgHistBuff.Oldest()
+	lastAvgValue := avg.avgHistBuff.Tail()
 	avg.avgSum = (avg.avgSum - lastAvgValue) + value
 
 	if avg.count < avg.period {
@@ -50,10 +53,10 @@ func (avg *MovingAverage) avg(value float64) {
 		avg.average = avg.avgSum / float64(avg.period)
 	}
 
-	avg.avgHistBuff.Add(value)
+	avg.avgHistBuff.Push(value)
 
 	value2 := value * value
-	last2AvgValue := avg.varHistBuff.Oldest()
+	last2AvgValue := avg.varHistBuff.Tail()
 	avg.avg2Sum = (avg.avg2Sum - last2AvgValue) + value2
 
 	n := float64(avg.period)
@@ -63,49 +66,5 @@ func (avg *MovingAverage) avg(value float64) {
 
 	avg.variance = ((n * avg.avg2Sum) - (avg.avgSum * avg.avgSum)) / (n * (n - 1))
 
-	avg.varHistBuff.Add(value2)
-}
-
-type historyBuffer struct {
-	buff []float64
-	head int
-	tail int
-	size int
-}
-
-func NewHistoryBuffer(size int) *historyBuffer {
-
-	hb := &historyBuffer{}
-	hb.size = size
-	hb.buff = make([]float64, hb.size+1)
-	hb.head = 0
-	hb.tail = 1
-	return hb
-}
-
-func (hb *historyBuffer) Add(value float64) {
-	hb.buff[hb.head] = value
-	hb.head++
-	hb.tail++
-
-	if hb.tail%(hb.size+1) == 0 {
-		hb.tail = 0
-	}
-
-	if hb.head%(hb.size+1) == 0 {
-		hb.head = 0
-	}
-
-}
-
-func (hb *historyBuffer) Oldest() float64 {
-	return hb.buff[hb.tail]
-}
-
-func (hb *historyBuffer) Last() float64 {
-	if hb.head == 0 {
-		return hb.buff[hb.size-1]
-	}
-	return hb.buff[hb.head-1]
-
+	avg.varHistBuff.Push(value2)
 }
--- movingstats.go
+++ movingstats.go
@@ -1,24 +1,58 @@
 package movingstats
 
+import "github.com/VividCortex/ewma"
+
 type MovingStats struct {
-	movingAverage *MovingAverage
+	sma   *MovingAverage
+	sma20 *MovingAverage
+	sEma  ewma.MovingAverage
+	dEma  ewma.MovingAverage
+	tEma  ewma.MovingAverage
+
+	//MACD
+	emaMacd9 ewma.MovingAverage
+	ema12    ewma.MovingAverage
+	ema26    ewma.MovingAverage
+	macd     float64
 }
 
 func NewMovingStats(size int) *MovingStats {
+	window := float64(size)
 	mstats := &MovingStats{}
-	mstats.movingAverage = NewAverage(size)
+	mstats.sma = NewAverage(size)
+	mstats.sma20 = NewAverage(size * 20)
+	mstats.sEma = ewma.NewMovingAverage(window)
+	mstats.dEma = ewma.NewMovingAverage(window)
+	mstats.tEma = ewma.NewMovingAverage(window)
+
+	mstats.emaMacd9 = ewma.NewMovingAverage(window * 9)
+	mstats.ema12 = ewma.NewMovingAverage(window * 12)
+	mstats.ema26 = ewma.NewMovingAverage(window * 26)
 
 	return mstats
 }
 
 func (ms *MovingStats) Add(value float64) {
-	ms.movingAverage.Add(value)
+	ms.sma.Add(value)
+	ms.sma20.Add(value)
+	ms.sEma.Add(value)
+	ms.dEma.Add(value)
+	ms.tEma.Add(value)
+
+	ms.emaMacd9.Add(value)
+	ms.ema12.Add(value)
+	ms.ema26.Add(value)
+
 }
 
 func (ms *MovingStats) SimpleMovingAverage() float64 {
-	return ms.movingAverage.SimpleMovingAverage()
+	return ms.sma.SimpleMovingAverage()
 }
 
 func (ms *MovingStats) MovingStandardDeviation() float64 {
-	return ms.movingAverage.MovingStandardDeviation()
+	return ms.sma.MovingStandardDeviation()
+}
+
+func (ms *MovingStats) MovingStandardDeviation20() float64 {
+	return ms.sma20.MovingStandardDeviation()
 }
